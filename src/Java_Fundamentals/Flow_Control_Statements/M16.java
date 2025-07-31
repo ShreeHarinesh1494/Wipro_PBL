684d91efbeef82d29e22cbf3f58edcbc24227a34
Shree Harinesh S <shreeharineshs@gmail.com>
2025-07-31 19:59:30 +0530

    Completed till Flow_Control_Statements

--- src/Java_Fundamentals/Flow_Control_Statements/M16.java
+++ src/Java_Fundamentals/Flow_Control_Statements/M16.java
@@ -1,6 +1,24 @@
 package Java_Fundamentals.Flow_Control_Statements;
-
+import java.util.*;
 public class M16
 {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a number to reverse: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reversed number of " + originalNumber + " is: " + reversed);
+
+        scanner.close();
+    }
 }
